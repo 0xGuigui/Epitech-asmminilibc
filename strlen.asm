e54fceedba692e11de16b68acf1c1dd871256312
mflr0 <mflr0@onionmail.org>
2022-02-21 23:07:09 +0100

    [+] - Added STRLEN

---
+++ strlen.asm
@@ -0,0 +1,21 @@
+;Strlen in ASM
+
+bits 64
+
+section .text
+    global strlen:function
+
+strlen:
+    mov rcx, 0          ; Set RCX à 0
+    jmp looping         ; On exécute la boucle du strlen
+
+looping:
+    cmp BYTE [rdi], 0   ; rdi == string, [] permet de déréférencer, 0 limite == '\0'
+    je end              ; Si rdi == '\0' suite a son incrémentation, on execute la condition de fin car JE = Jump Equal et rdi = '\0'
+    inc rdi             ; Incrémentation de RDI pour avancer dans la string
+    inc rcx             ; Increment RCX pour compter le nombre de fois qu'on fait la boucle
+    jmp looping         ; Boucle tant que str[rcx] != \0
+
+end:
+    mov rax, rcx        ; On bouge la valeur de RCX dans RAX
+    ret                 ; Return de RAX
