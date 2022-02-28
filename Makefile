4e4bfab8b1b20cda65507802be94f37342b44147
mflr0 <mflr0@onionmail.org>
2022-02-28 15:42:31 +0100

    [+] - Initial push

--- Makefile
+++ Makefile
@@ -5,22 +5,25 @@
 ## Makefile for libasm.so
 ##
 
-SRC        =    strlen.asm
+SRC		=	strlen.asm	\
+			memcpy.asm	\
+			memset.asm	\
+			strchr.asm
 
-OBJ        =    $(SRC:.asm=.o)
+OBJ		=    $(SRC:.asm=.o)
 
 NAME    =    libasm.so
 
 all:    $(OBJ)
-        ld -shared -o $(NAME) $(OBJ)
+		ld -shared -o $(NAME) $(OBJ)
 
 %.o: %.asm
-        nasm -f elf64 $< -o $@
+		nasm -f elf64 $< -o $@
 
 clean:
-    rm -f $(OBJ)
+	rm -f $(OBJ)
 
 fclean:clean
-    rm -f $(NAME)
+	rm -f $(NAME)
 
 re:    fclean all
---
+++ strchr.asm
@@ -0,0 +1,29 @@
+;Strchr in ASM
+
+bits 64
+
+section .text
+    global strchr:function
+
+strchr:
+    mov rcx, 0              ;initialiser le compteur de caractère
+    jmp looping             ;on jump jusqu'a looping pour commencer
+
+looping:
+    cmp BYTE [rdi], 0       ;On compare le caractere avec 0 (fin de chaine)
+    je end                  ;Si c'est egal on jump vers end (fin de la fonction)
+    cmp BYTE [rdi], sil     ;On compare avec l'argument sil (caractere a chercher)
+    je found                ;Si c'est egal on jump vers found (caractere trouve)
+    inc rdi                 ;On incremente le pointeur de chaine (pointeur vers le caractere suivant)
+    inc rcx                 ;On incremente le compteur de boucle (compteur de caractere)
+    jmp looping             ;On jump jusqu'a looping (boucle)
+
+found:
+    mov rax, rdi            ;On stocke le pointeur dans rax (pointeur de chaine)
+    mov rdi, rcx            ;On stocke le compteur dans rdi (compteur de boucle)
+    ret                     ;On retourne
+
+end:
+    mov rax, 0              ;On stocke 0 dans rax (pointeur de chaine)
+    mov rdi, rcx            ;On stocke le compteur dans rdi (compteur de boucle)
+    ret                     ;On retourne
