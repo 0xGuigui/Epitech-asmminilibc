5b47c1d1b9aa7a2ee6ede4afc4191901c93b5efc
Nysioko <guilhem.jehanno@epitech.eu>
2022-03-02 00:12:08 +0100

    [+] - Reworked some function and added strcmp

--- memcpy.asm
+++ memcpy.asm
@@ -8,17 +8,21 @@ section .text
 memcpy:
 	mov rcx, 0				; On initialise le compteur de boucle
 	cmp rdx, 0 				; On compare le nombre de caractères à copier avec 0
-	je ending					; Si le nombre de caractères à copier est égal à 0, on sort de la fonction
+	je ending				; Si le nombre de caractères à copier est égal à 0, on sort de la fonction
     jmp looping				; Sinon on passe à la boucle
 
 looping:
     mov r8, rcx				; On stocke le compteur de boucle dans r8
-	mov al, [rsi + r8]		; On récupère le caractère à copier dans al
-	mov [rdi + r8], al		; On copie le caractère dans le pointeur d'arrivée en lui donnant la valeur de al
+	mov r9, r8				; On stocke le compteur de boucle dans r9
+	add r9, rsi				; On ajoute le pointeur de départ à r9
+	mov al, [r9]			; On stocke le caractère à copier dans al
+	mov r10, r8				; On stocke le compteur de boucle dans r10
+	add r10, rdi			; On ajoute le pointeur de destination à r10
+	mov [r10], al			; On copie le caractère dans le pointeur d'arrivée en lui donnant la valeur de al
 	inc rcx					; On incrémente le compteur de boucle
 	cmp rdx, rcx			; On compare le nombre de caractères à copier avec le compteur de boucle (on vérifie que le compteur de boucle n'a pas atteint le nombre de caractères à copier)
 	jne looping				; Si le compteur de boucle n'est pas égal au nombre de caractères à copier, on passe à la boucle
-	jmp ending					; Sinon on sort de la fonction
+	jmp ending				; Sinon on sort de la fonction
 
 ending:
 	mov rax, rdi			; On stocke le pointeur d'arrivée dans rax (on retourne le pointeur d'arrivée)
---
+++ strcmp.asm
@@ -0,0 +1,28 @@
+;Strcmp in ASM
+
+bits 64
+
+section .text
+    global strcmp:function
+
+strcmp:
+    mov rax, 0                      ;initialiser rax à 0
+    jmp looping                     ;on va dans la boucle
+
+looping:
+    mov cl, BYTE[rsi]               ;on prend le caractère de la chaine 1
+    cmp BYTE[rdi], cl               ;compare les deux caractères de la chaine
+    jne end                         ;si les deux adresses ne sont pas égales, on sort de la boucle
+    cmp byte [rdi], 0               ;on compare le caractère de la première adresse
+    je end                          ;si le caractère est nul, on sort de la boucle
+    cmp byte [rsi], 0               ;on compare le caractère de la deuxième adresse
+    je end                          ;si le caractère est nul, on sort de la boucle
+    inc rdi                         ;on incrémente rdi
+    inc rsi                         ;on incrémente rsi
+    jmp looping                     ;on recommence la boucle
+
+end:
+    movzx rax, BYTE[rdi]            ;on récupère le caractère de la première adresse
+    movzx rcx, BYTE[rsi]            ;on récupère le caractère de la deuxième adresse
+    sub rax, rcx                    ;on soustrait les deux caractères de la chaine
+    ret                             ;on retourne rax
