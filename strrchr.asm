d699bd81869bb001324ac40054cdd693638d26d1
mflr0 <mflr0@onionmail.org>
2022-03-02 16:16:28 +0100

    [+] - Fixed crash

--- strrchr.asm
+++ strrchr.asm
@@ -6,21 +6,23 @@ section .text
     global strrchr:function
 
 strrchr:
-    mov rax, 0              ; On stocke 0 dans rax (pointeur de chaine)
-    jmp looping             ; On jump jusqu'a looping pour commencer
+    mov rax, 0              ;On stocke 0 dans rax (pointeur de chaine)
+    jmp looping             ;on jump jusqu'a looping pour commencer
 
 looping:
-    cmp BYTE [rdi], sil     ; On compare avec l'argument sil (caractere a chercher)
-    je found                ; Si c'est egal on jump vers found (caractere trouve)
-    cmp BYTE [rdi], 0       ; On compare le caractere avec 0 (fin de chaine)
-    je ending               ; Si c'est egal on jump vers ending (fin de la fonction)
-    inc rdi                 ; On incremente le pointeur de chaine (pointeur vers le caractere suivant)
-    jmp looping             ; On jump jusqu'a looping (boucle)
+    cmp BYTE [rdi], sil     ;On compare avec l'argument sil (caractere a chercher)
+    je found                ;Si c'est egal on jump vers found (caractere trouve)
+    cmp BYTE [rdi], 0       ;On compare le caractere avec 0 (fin de chaine)
+    je ending               ;Si c'est egal on jump vers ending (fin de la fonction)
+    inc rdi                 ;On incremente le pointeur de chaine (pointeur vers le caractere suivant)
+    jmp looping             ;On jump jusqu'a looping (boucle)
 
 found:
-    mov rax, rdi            ; On stocke le pointeur dans rax (pointeur de chaine)
-    inc rdi                 ; On incremente le pointeur de chaine (pointeur vers le caractere suivant)
-    jmp looping             ; On jump jusqu'a looping (boucle)
+    mov rax, rdi            ;On stocke le pointeur dans rax (pointeur de chaine)
+    cmp BYTE[rdi], 0        ;On compare le caractere avec 0 (fin de chaine)
+    je ending               ;Si c'est egal on jump vers ending
+    inc rdi                 ;On incremente le pointeur de chaine (pointeur vers le caractere suivant)
+    jmp looping             ;On jump jusqu'a looping (boucle)
 
 ending:
-    ret                     ; On retourne
+    ret                     ;On retourne
